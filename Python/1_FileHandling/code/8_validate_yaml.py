8b88d453b82f365d5be980d3a3d4661ff342b2ed
Babu S <babus@Babus-MacBook-Pro.local>
2025-09-07 21:42:00 +0530

    Updated the repo

--- Python/1_FileHandling/code/8_validate_yaml.py
+++ Python/1_FileHandling/code/8_validate_yaml.py
@@ -10,7 +10,14 @@ def validate_yaml(path: str) -> None:
     
     with file_path.open('r', encoding="utf=8") as fy:
         content = yaml.safe_load(fy)
-        print(content)
+        lst = content["required_keys"]
+        print(lst)
+        for value in content.values():            
+            for val in value:
+              print(val)
+
+
+                
 
 if __name__ == "__main__":
     validate_yaml("/Users/babus/Desktop/repos/InterviewPrep/Python/1_FileHandling/sample_files/validate_yaml.yaml")
