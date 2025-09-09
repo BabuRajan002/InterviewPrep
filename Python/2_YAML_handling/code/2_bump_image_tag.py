15ac30c7c38f5ff18977f6353b5355083c2e8b87
Babu S <babus@Babus-MacBook-Pro.local>
2025-09-09 15:27:55 +0530

    Updated the repo

--- Python/2_YAML_handling/code/2_bump_image_tag.py
+++ Python/2_YAML_handling/code/2_bump_image_tag.py
@@ -1,17 +1,34 @@
-import yaml
 from ruamel.yaml import YAML
-yaml = YAML(typ="rt")
-yaml.indent(mapping=2, sequence=2, offset=2)
+from pathlib import Path
 
-# with open("/Users/babus/Desktop/repos/InterviewPrep/Python/2_YAML_handling/sample_files/manifests.yaml", encoding="utf=8") as f:
-#     doc = list(yaml.safe(f))
+yaml = YAML(typ="rt")       # round-trip mode: preserve formatting & comments
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.width = 4096
+yaml.preserve_quotes = True
+# yaml.explicit_start = True
 
-docs = list(yaml.safe_load_all(open("/Users/babus/Desktop/repos/InterviewPrep/Python/2_YAML_handling/sample_files/manifests.yaml", "r", encoding="utf-8")))
+path = Path("/Users/babus/Desktop/repos/InterviewPrep/Python/2_YAML_handling/sample_files/manifests.yaml")
 
+# Load all docs
+with path.open(encoding="utf=8") as f:
+    docs = list(yaml.load_all(f))
+
+# Modify in place
 for d in docs:
-# if doc["spec"]["template"]["spec"]["containers"][0]["image"] == ""
- print(d["spec"]["template"]["spec"]["containers"][0]["image"])
-   
+    containers = d.get("spec", {}).get("template", {}).get("spec", {}).get("containers", [])
+    for c in containers:
+        img = c.get("image")
+        if img and "ghcr.io/acme/" in img:
+            # strip any old tag and force :latest
+            base = img.split("@", 1)[0]  # remove digest if any
+            last_colon = base.rfind(":")
+            last_slash = base.rfind("/")
+            print(last_colon,last_slash)
+            if last_colon > last_slash:   # only strip if colon is after last slash
+                base = base[:last_colon]
+                
+            c["image"] = f"{base}:latest"
 
-
-
+# Write back to original file
+with path.open("w", encoding="utf=8") as f:
+    yaml.dump_all(docs, f)
