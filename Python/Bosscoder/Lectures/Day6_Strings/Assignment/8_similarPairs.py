8cc9d9c7c97bb8b79bc70b32647af0850ce65818
Babu S <babus@Babus-MacBook-Pro.local>
2025-12-12 14:41:20 +0530

    Updated the master

--- Python/Bosscoder/Lectures/Day6_Strings/Assignment/8_similarPairs.py
+++ Python/Bosscoder/Lectures/Day6_Strings/Assignment/8_similarPairs.py
@@ -1,25 +1,27 @@
 class SimilarPairs:
     def __init__(self, arr):
         self.arr = arr
-        
+    
     def find(self):
-        arr = self.arr
-        freq = {}
-        for word in arr:
-            key = "".join(sorted(set(word)))
-
-            if key in freq:
-                freq[key] += 1
-            else:
-                freq[key] = 1
-        # return freq
-        print(freq)
         count = 0
-        for k in freq.values():
-            if k > 1:
-                count += (k * (k - 1)) // 2
-        return count            
+        n = len(self.arr)
+        words = self.arr
+
+        for i in range(n):
+            s = words[i]
+            st = set(s)
+            print(st)
+
+            for j in range(i+1, n):
+                t = words[j]
+                temp = set(t)
+                print(temp)
+
+                if st == temp:
+                    count += 1
+        return count       
+         
     
 if __name__ == "__main__":
-    sp = SimilarPairs(["aabb","ab","ba"])
+    sp = SimilarPairs(["aba","aabb","abcd","bac","aabc"])
     print(sp.find())
